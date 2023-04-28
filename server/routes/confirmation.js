9503a380f3d8384d1fa7d433a1d63c28afa3f3cb
Richard Romero <qricardo@outlook.com>
2023-04-28 12:29:43 -0600

    Update table names in query to avoid conflicts with different schemas names

--- server/routes/confirmation.js
+++ server/routes/confirmation.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-const {performQuery} = require('../public/javascripts/db_query')
-const {JSONError} = require("../public/javascripts/cje");
+const {performQuery} = require('../public/javascripts/db_query');
+const {JSONError} = require('../public/javascripts/cje');
 
 const maxConfigTime = parseInt(process.env.USR_CONFIG_MAX_TIME, 10) || 30;
 
@@ -15,7 +15,7 @@ router.get('/:id_config', async function (req, res, next) {
             // This query returns the time difference in minutes
             const query = {
                 name: 'signupConfQuery',
-                text: 'SELECT sp.usr_id,EXTRACT(EPOCH FROM (NOW() - sp.timestamp)) / 60 AS diff FROM test_db_living.public.signup_mail AS sp WHERE sp.url = $1',
+                text: 'SELECT sp.usr_id,EXTRACT(EPOCH FROM (NOW() - sp.timestamp)) / 60 AS diff FROM public.signup_mail AS sp WHERE sp.url = $1',
                 values: [req.params.id_config]
             }
 
@@ -30,7 +30,7 @@ router.get('/:id_config', async function (req, res, next) {
                 } else {
                     const updateVerifyQuery = {
                         name: 'updateVerifyQuery',
-                        text: 'UPDATE test_db_living.public.user AS sp SET verified=1 WHERE id = $1;',
+                        text: 'UPDATE public.user AS sp SET verified=1 WHERE id = $1;',
                         values: [result.rows[0].usr_id]
                     }
 
@@ -38,7 +38,7 @@ router.get('/:id_config', async function (req, res, next) {
 
                     const verifyDeleteQuery = {
                         name: 'verifyQuery',
-                        text: 'DELETE FROM test_db_living.public.signup_mail AS sp WHERE sp.url = $1;',
+                        text: 'DELETE FROM public.signup_mail AS sp WHERE sp.url = $1;',
                         values: [req.params.id_config]
                     }
 
@@ -49,7 +49,7 @@ router.get('/:id_config', async function (req, res, next) {
                             code: 0,
                             message: 'e-mail confirmation was successfully done'
                         }
-                    ))
+                    ));
                 }
             }
         } catch (error) {
