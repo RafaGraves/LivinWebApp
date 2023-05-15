e69d65e60b9dfce3cb4e3bdd41245f274851f52d
Richard Romero <qricardo@outlook.com>
2023-05-14 20:49:04 -0600

    Add the possibility to resize a user image directly from the client request

--- server/app/Http/Controllers/LocalImagesController.php
+++ server/app/Http/Controllers/LocalImagesController.php
@@ -10,6 +10,7 @@ use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Str;
 use Illuminate\Validation\ValidationException;
+use Intervention\Image\Facades\Image;
 
 class LocalImagesController extends Controller
 {
@@ -19,6 +20,7 @@ class LocalImagesController extends Controller
     {
         try {
             $sessionUsrId = $this->accessUserIdFromAuthToken();
+
 
             $queryData = UserData::query()
                 ->select('usr2_foto')
@@ -31,8 +33,35 @@ class LocalImagesController extends Controller
                 $filePath = $this->emptyUserPhoto;
             }
 
-            return response(Storage::get($filePath), 200)
-                ->header('Content-type', Storage::mimeType($filePath));
+            try {
+
+                $command = request()->query('command');
+
+
+                $image = Image::make(Storage::get($filePath));
+                if ($command !== null) {
+                    switch ($command) {
+                        case 'resize':
+                            $width = (int)request()->query('w') ;
+                            $height = (int)request()->query('h');
+                            $image->resize
+                            (
+                                $width != null ? $width : $image->width(),
+                                $height != null ? $height : $image->height()
+                            );
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                return response()->json(
+                    [
+                        'data' => $image->encode('data-url')->encoded
+                    ]
+                );
+            } catch (Exception $e) {
+                return response()->json(['status' => -1, 'message' => 'error'], 422);
+            }
 
 
         } catch (Exception $e) {
